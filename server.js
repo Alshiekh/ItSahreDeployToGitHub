de0b00037bb825fb9be6474f5d8fa839a812defb
alshiekh <alshiekhosman4@gmail.com>
2019-08-30 07:45:32 +0300

    updates to deploy to Server heroku1

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app-Deploy'));
+app.use(express.static(__dirname + '/dist/ItShareShop'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/app-Deploy/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ItShareShop/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
